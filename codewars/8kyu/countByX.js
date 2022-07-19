516af3e59166674248c32ca00d6f89f9d14b8663
Maddy Ali <maddyoali@gmail.com>
2022-07-19 06:09:31 -0500

    Return an array of the first num multiplies of x

---
+++ codewars/8kyu/countByX.js
@@ -0,0 +1,30 @@
+// Create a function with two arguments that will return an array of the first (n) multiples of (x).
+
+// Assume both the given number and the number of times to count will be positive numbers greater than 0.
+
+// Return the results as an array (or list in Python, Haskell or Elixir).
+
+/*
+P: two nums
+R: array of first num (n) multiples of second num (x)
+E: countBy(1,10) => [1,2,3,4,5,6,7,8,9,10]
+   countBy(2,5) => [2,4,6,8,10]
+P: create empty array
+loop through n, starting from 0 (bc 0 * anything = 0)
+multiply x * i
+push to empty array
+*/
+
+function countBy(x, n) {
+  let z = [];
+  for (let i = 1; i <= n; i++) {
+    z.push(x * i);
+  }
+  return z;
+}
+
+/*
+Other solutions:
+const countBy = (x, n) => Array.from({length: n}, (v, k) => (k + 1) * x)
+
+*/
