2aef9d38d39b0c387fd8e0439561ed19098cd770
Maddy Ali <maddyoali@gmail.com>
2022-07-18 19:07:03 -0500

    Return opposite int

---
+++ codewars/8kyu/oppositeNum.js
@@ -0,0 +1,12 @@
+// Very simple, given an integer or a floating-point number, find its opposite.
+
+/* 
+P: int
+R: opposite of int
+E: 1: -1
+14: -14
+-34: 34
+P: return opposite int by placing minus sign in front
+*/
+
+const opposite = (number) => -number;
