e643895c0cecb12fdeb38a5523651ad625dfb562
Maddy Ali <maddyoali@gmail.com>
2022-07-26 21:07:56 -0500

    Return min/max

---
+++ codewars/8kyu/minMax.js
@@ -0,0 +1,5 @@
+// Task
+// Write a function that returns both the minimum and maximum number of the given list/array.
+
+const minMax = (arr) => [Math.min(...arr), Math.max(...arr)];
+// using spread syntax '...' because min and max only work for LISTS
